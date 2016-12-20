c8ccb7d54da6f0b4edfc09db21f9a4eb3415e783
JSON <838432498@qq.com>
2016-12-20 17:44:28 +0800

    s

--- src/pages/client/login/signup.vue
+++ src/pages/client/login/signup.vue
@@ -3,27 +3,31 @@
         <p>用户名：<input type="text" v-model="username" value=""></p>
         <p>密码：<input type="password" v-model="password" value=""></p>
         <p>确认密码：<input type="password" v-model="repassword" value=""></p>
+        <p>你的邮箱：<input type="text" v-model="email" value=""></p>
         <p><button @click="signup">注册</button></p>
     </div>
 </template>
 <script>
+    import api from 'src/api'
     export default {
         data() {
             return {
                 username: '',
                 password: '',
-                repassword: ''
+                repassword: '',
+                email: ''
             }
         },
         methods: {
-            signup() {
-                setTimeout(() => {
-                   // console.log('过了3秒')
-                     window.close()
-                     //只能关闭用脚本打开的页面
-                     //window.onbeforeunload
-                     //当窗口即将被卸载时,会触发该事件.此时页面文档依然可见,且该事件的默认动作可以被取消
-                }, 3000)
+            async signup() {
+                var {data:{code, msg}} = await api.post('/user/signup', {
+                    username: this.username,
+                    password: this.password,
+                    email: this.email
+                })
+                if(code === 200) {
+                    alert('注册成功')
+                }
             }
         }
     }
